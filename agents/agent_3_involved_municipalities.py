3ad56097d84a85dc31edf3f9ca91750f1eef6505
Mirco Rudolph <mirco@openearth.org>
2025-06-12 17:27:18 -0600

    Revert "chore: exchanged tavily tool with buildt in openai search tool"
    This reverts commit c352feabf334f7b4ff6a2cc01dc483ed26390be4.

--- agents/agent_3_involved_municipalities.py
+++ agents/agent_3_involved_municipalities.py
@@ -3,18 +3,15 @@ from langgraph.prebuilt import create_react_agent
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from state.agent_state import AgentState
 from langchain_openai import ChatOpenAI
-
-# Define tools for the agent
-tools = []
-
-# Create the agents
-model = ChatOpenAI(
-    model="gpt-4o",
-    model_kwargs={
-        "tools": [{"type": "web_search_preview"}],
-    },  # Built-in search tool from OpenAI
+from tools.tools import (
+    search_municipalities_tool,
 )
 
+# Create the agents
+model = ChatOpenAI(model="gpt-4o", temperature=0.0, seed=42)
+
+# Define tools for the agent
+tools = [search_municipalities_tool]
 
 system_prompt_agent_3 = SystemMessage(
     """
@@ -36,7 +33,7 @@ Follow these guidelines carefully to complete the task:
 3. Review the introduction for the climate action implementation plan.
 4. Review the sub-actions for implementing the climate action.
 5. Use the provided tools to retrieve municipal institutions for the implementation of the specific climate action for the given city.
-    - Ensure that the retrieved information is relevant to the action and the city. This means only search for institutions highly relevant to the action and institutions within the city, or state or the city.
+    - Ensure that the retrieved information is relevant to the action and the city.
     - If you can not retrieve relevant information for a specific part, **DO NOT** include this fact in the output. 
     - **Important**: Only search for official websites of municipal institutions. Do not use channels like Instagram, Facebook, Twitter, LinkedIn, etc.
 </task>
